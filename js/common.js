5ee7b1422faeba464c36f1be9f7d482698c9478f
shontanakamura <63178332+shontanakamura@users.noreply.github.com>
2021-01-15 13:39:24 +0900

    test

---
+++ js/common.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(function(){
+  $('.target__inner').on('click', function () {
+      $('.target__inner, .nav-right, .target__inner__line, .target, .nav, .ichath-txt').toggleClass('show');
+  });
+  })
